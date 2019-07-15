da013216ad425119678b9b63afed96645a89cb25
Hiroshi Kono <hiroshi.kono@gmail.com>
2019-07-15 17:04:46 +0900

    Change method name and variable name from '...fields' to '..columns'

--- lib/format_sql.rb
+++ lib/format_sql.rb
@@ -6,13 +6,15 @@ class FormatSql
     # @order by
     def initialize(sql_str)
         @sql = sql_str
+        @select_columns = get_select_columns
     end
 
-    def get_select_fields
+    # SQL文からselectされているカラムを抜き出して配列に入れて返す
+    def get_select_columns
         sql_downcase = @sql.downcase
         tokens_downcase = sql_downcase.gsub(/\,/, ' ').split("\s")
         tokens = @sql.gsub(/\,/, ' ').split("\s")
-        select_fields=[]
+        select_columns=[]
         if sql_downcase.include?('distinct')
             pos = 2
         else
@@ -20,10 +22,21 @@ class FormatSql
         end
         end_pos = tokens_downcase.index('from') -1
         while pos <= end_pos
-            select_fields.push tokens[pos]
+            select_columns.push tokens[pos]
             pos += 1
         end
-        select_fields
+        select_columns
+    end
+
+    # selectされているカラムの配列からGroup by の対象になるカラムの配列を作って返す
+    def get_group_by_columns
+        group_by_columns = []
+        @select_columns.each do |col|
+            unless col.downcase.include?('sum(')
+                group_by_columns.push col
+            end 
+        end
+        group_by_columns
     end
 
 end
--- spec/format_sql_spec.rb
+++ spec/format_sql_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe FormatSql do
     let(:target) { FormatSql.new('select Fld1, Fld2,Fld3,sum(fld11),sum(fld12) from [table] ') }
     it "can read sql" do
-        expect(target.get_select_fields).to eq ['Fld1','Fld2','Fld3','sum(fld11)','sum(fld12)']
+        expect(target.get_select_columns).to eq ['Fld1','Fld2','Fld3','sum(fld11)','sum(fld12)']
+        expect(target.get_group_by_columns).to eq ['Fld1','Fld2','Fld3']
     end
 end
