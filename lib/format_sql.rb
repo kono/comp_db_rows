4361794b1827cc78399444e43af852ab141d7b59
Hiroshi Kono <hiroshi.kono@gmail.com>
2019-07-15 16:41:34 +0900

    Add get_select_fields method in format_sql.rb

--- lib/format_sql.rb
+++ lib/format_sql.rb
@@ -8,4 +8,22 @@ class FormatSql
         @sql = sql_str
     end
 
+    def get_select_fields
+        sql_downcase = @sql.downcase
+        tokens_downcase = sql_downcase.gsub(/\,/, ' ').split("\s")
+        tokens = @sql.gsub(/\,/, ' ').split("\s")
+        select_fields=[]
+        if sql_downcase.include?('distinct')
+            pos = 2
+        else
+            pos = 1
+        end
+        end_pos = tokens_downcase.index('from') -1
+        while pos <= end_pos
+            select_fields.push tokens[pos]
+            pos += 1
+        end
+        select_fields
+    end
+
 end
--- spec/format_sql_spec.rb
+++ spec/format_sql_spec.rb
@@ -2,5 +2,6 @@ RSpec.describe CompDbRows do
     let(:target) { FormatSql.new('select fld1, sum(fld2) from [table] ') }
     it "can read sql" do
         expect(target.instance_variable_get(:@sql)).to eq 'select fld1, sum(fld2) from [table] '
+        expect(target.get_select_fields).to eq ['fld1','sum(fld2)']
     end
 end
