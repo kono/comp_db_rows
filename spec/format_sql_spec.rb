8f31d2a4c9dbbc5eaffe11795e0f38021f86d9c0
Hiroshi Kono <hiroshi.kono@gmail.com>
2019-07-15 16:47:30 +0900

    Fix rspec describe

--- spec/format_sql_spec.rb
+++ spec/format_sql_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe CompDbRows do
+RSpec.describe FormatSql do
     let(:target) { FormatSql.new('select Fld1, Fld2,Fld3,sum(fld11),sum(fld12) from [table] ') }
     it "can read sql" do
         expect(target.get_select_fields).to eq ['Fld1','Fld2','Fld3','sum(fld11)','sum(fld12)']
